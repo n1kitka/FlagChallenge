fc5d29af741e45dc6f0272be88dba6da0a8cb419
Nikita <98713485+n1kitka@users.noreply.github.com>
2025-01-23 11:25:59 +0200

    Initial Commit

---
+++ FlagChallenge/FlagChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagChallengeApp.swift
+//  FlagChallenge
+//
+//  Created by Nikita on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

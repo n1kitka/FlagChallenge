37fb812eb7cec8ca82c9f1d2ca820371b444cdac
Nikita <98713485+n1kitka@users.noreply.github.com>
2025-01-23 14:52:35 +0200

    Added AppState

---
+++ FlagChallenge/AppState/AppState.swift
@@ -0,0 +1,13 @@
+//
+//  AppState.swift
+//  FlagChallenge
+//
+//  Created by Nikita on 23.01.2025.
+//
+
+enum AppState {
+    case initial
+    case question(Int)
+    case feedback
+    case finished
+}
